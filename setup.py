6984c14288b8d4c20eb81c180ea2b4f2728f37fb
Jamie Wilkinson <jaq@spacepants.org>
2013-03-14 18:29:27 +1100

    Set version 0.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 VERSION_MAJOR = 0
-VERSION_MINOR = 6
+VERSION_MINOR = 7
 pymad_version = str(VERSION_MAJOR) + "." + str(VERSION_MINOR)
 
 def get_setup():
